65d92a00f5a80dc8f4acd364072943880ced96a6
WahyuSapurata <wm337708@gmail.com>
2024-03-15 11:09:46 +0800

    tes

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -154,6 +154,7 @@ class Dashboard extends BaseController
         // Menampilkan OperasionalKantor berdasarkan lokasi user dengan melakukan join
         $dataFee = $dataFees->map(function ($item) use ($dataUser) {
             $dataClient = DataClient::where('uuid', $item->uuid_client)->first();
+            dd($dataClient);
             $user = $dataUser->where('uuid', $dataClient->uuid_user)->first();
             $item->lokasi_user = $user->lokasi;
             return $item;
