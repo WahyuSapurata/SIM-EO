b522efc023e9250b717267b840e0d42e7cb063b5
WahyuSapurata <wm337708@gmail.com>
2024-04-08 17:35:31 +0800

    tes

--- app/Http/Controllers/PiutangController.php
+++ app/Http/Controllers/PiutangController.php
@@ -32,8 +32,8 @@ class PiutangController extends BaseController
             $dataUser = User::where('uuid', $item->uuid_user)->first();
             $persetujuanInvoice = PersetujuanInvoice::where('uuid', $item->uuid_persetujuanInvoice)->first();
             $invoice = Invoice::where('uuid', $persetujuanInvoice->uuid_invoice)->first();
-            dd($invoice);
             $client = DataClient::where('uuid', optional($invoice)->uuid_vendor)->first();
+            dd($client);
 
             $item->no_invoice = optional($invoice)->no_invoice;
             $item->tanggal_invoice = optional($invoice)->tanggal_invoice;
